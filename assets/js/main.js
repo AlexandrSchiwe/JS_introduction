54df5a054411d2f46ce5cb23a39c1810ba58e18b
githubsupercode <alex.supercode@gmail.com>
2022-04-14 09:01:08 +0200

    finalpush

---
+++ assets/js/main.js
@@ -0,0 +1,163 @@
+// Level 1.1 Console
+
+// a = 12;
+// b = a * 3;
+// console.log(b);
+// document.write(b);
+
+// console.log(document.URL);
+
+// console.log("Hello World!");
+
+// console.log("Anton");
+
+// let name = "Anton";
+// console.log(name);
+
+// let age = (28);
+// console.log(age);
+
+// let job = "Trainer";
+// console.log(job);
+
+// let married = true;
+// console.log(married);
+
+// console.log(name + " ist " + age + " Jahre alt, ist von Beruf " + job + " und ist verheiratet:" + married)
+
+// Level 1.1 Einführung
+
+
+// function myFunction() {
+//     document.getElementById("test").style.color = "red";
+//   }
+
+// Level 1.2
+
+// // This is a single line JavaScript comment
+// document.write("I have comments in my JavaScript code!");
+// //document.write("You can't see this!");
+// //-->
+
+// document.write("I have multi-line comments!");
+// /*document.write("You can't see this!");
+// document.write("You can't see this!");
+// document.write("You can't see this!");
+// document.write("You can't see this!");
+// document.write("You can't see this!");
+// document.write("You can't see this!");
+// document.write("You can't see this!");*/
+
+// Level 1.4
+
+// window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?"); 
+
+// let alter = prompt( "Gib mir bitte dein Alter:" );
+// console.log(alter);
+
+// let b = 5;
+// let a = b * 5 -3;
+// alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// alert(mann + grosse);
+
+// Level 1.5
+
+// let carName = "BMW";
+// console.log(carName);
+
+// let x = (150);
+// console.log(x);
+
+// let y = (50);
+// console.log(y);
+
+// let z = (x + y);
+// console.log(z);
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = (35);
+// console.log(firstName + lastName + age);
+
+// Level 1.72m
+
+// let helloWorld = "Hello World";
+// const pi = (3.14);
+// const burjDubai = (828);
+// let fullName = "Jan Schmidt";
+// const eifelTower = (324);
+// let camelCase = "camelCase";
+// const kursstatus = (true);
+
+// Level 1.11
+
+// let x = "Hello ";
+// let y = "World<br>";
+// let z = (x+y);
+// document.write(z);
+// let n = x+ " " + y;
+// document.write(n);
+// document.write(x=y);
+
+// let meinString = "Ich bin erster";
+// let b = "Ich bin zweiter";
+// let f = meinString + b;
+// document.write(f);
+
+// Level 1.12
+
+// let score = 0;
+// score += 10;
+// console.log("Ergebnis " + score);
+
+// let zahl = 1;
+// zahl++;
+// console.log("increment " + zahl)
+
+// let zahl = 1;
+// zahl--;
+// console.log("increment " + zahl)
+
+// Level 1.13
+
+// Addition
+
+// let addition_operator = 3746 + 8265;
+// console.log("addition " + addition_operator);
+
+// // Subtraktion
+
+// let subtraktion_operator = 120 - 98;
+// console.log("subtraktion " + subtraktion_operator);
+
+// // Multiplikation
+
+// let multiplication_operator = 12 * 4;
+// console.log("multiplication " + multiplication_operator);
+
+// // Division
+
+// let division_operator = 8 / 2;
+// console.log("division " + division_operator);
+
+// // Modul: zeigt an, was der Rest sein würde.
+
+// let modulus_operator = 136 % 6;
+// console.log("modulus " + modulus_operator)
+
+// Einführung Documents 1.1
+
+// document.write("<h1>Hello World!</h1>");
+// document.write("<p>Have a nice day!</p>");
+// let myText = "Hello again";
+// document.write(myText);
+// let firstName = "Alex";
+// document.write("<p>Name:" + firstName + "</p>" )
+// let lastName = "Schiwe";
+// document.write("<p>Nachname:" + lastName + "</p>" )
